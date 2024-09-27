e4c523aeaa92d25130be92ad25b7b3eb15ead85e
oliveiramsk <153978712+oliveiramsk@users.noreply.github.com>
2024-09-27 02:27:21 -0300

    Update GeneralSettingForm.jsx

--- frontend/src/modules/SettingModule/GeneralSettingsModule/forms/GeneralSettingForm.jsx
+++ frontend/src/modules/SettingModule/GeneralSettingsModule/forms/GeneralSettingForm.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Input, Form, Select, Switch } from 'antd';
 import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
 
-import languages from '@/locale/translation';
+import languages from '@/locale/translation/translation';
 import useLanguage from '@/locale/useLanguage';
 import { translateAction } from '@/redux/translate/actions';
 import { countryList } from '@/utils/countryList';
